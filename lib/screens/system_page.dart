515b8dd803b35846af3839b6bf8338283fc178f0
Vijay <44964055+vijayakumarmani2@users.noreply.github.com>
2025-01-25 08:30:25 +0530

    ..

--- lib/screens/system_page.dart
+++ lib/screens/system_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:hba1c_analyzer_1/widget/show_ip.dart';
 import 'package:hba1c_analyzer_1/widget/BottomNavigationBar.dart';
 import 'package:hba1c_analyzer_1/widget/wifi_configuration_widget.dart';
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_libserialport
-import path_provider_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLibserialportPlugin.register(with: registry.registrar(forPlugin: "FlutterLibserialportPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }

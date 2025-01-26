1944865fb310b9d7a3559383eae70b3afb54974c
Vijay <44964055+vijayakumarmani2@users.noreply.github.com>
2025-01-27 03:50:55 +0530

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:hba1c_analyzer_1/screens/welcomepage.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:window_manager/window_manager.dart';
+
 
 Future<void> main() async {
-
   // Initialize FFI for desktop platforms
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfi;
-  
-  
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    width:
+    MediaQuery.of(context).size.width;
+    height:
+    MediaQuery.of(context).size.height;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: WelcomePage(),

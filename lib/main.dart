4f1d9111cec9d1127d7b30cb62610954138802df
Vijay <44964055+vijayakumarmani2@users.noreply.github.com>
2025-01-27 11:39:59 +0530

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -8,27 +8,10 @@ import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 
 // Declare GTK functions
-final gtk_window_set_decorated = DynamicLibrary.open('libgtk-3.so')
-    .lookupFunction<Void Function(Pointer<Void>, Int32), void Function(Pointer<Void>, int)>('gtk_window_set_decorated');
 
-final gtk_window_fullscreen = DynamicLibrary.open('libgtk-3.so')
-    .lookupFunction<Void Function(Pointer<Void>), void Function(Pointer<Void>)>('gtk_window_fullscreen');
-
-// Modify the window
-void configureWindow(Pointer<Void> windowPointer) {
-  gtk_window_set_decorated(windowPointer, 0); // Remove decorations
-  gtk_window_fullscreen(windowPointer);      // Fullscreen mode
-}
 
 void main() async {
- WidgetsFlutterBinding.ensureInitialized();
-SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-// to hide only bottom bar:
-SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-  statusBarColor: Colors.transparent,
-  systemNavigationBarColor: Colors.transparent,
-  statusBarIconBrightness: Brightness.light,
-));
+ 
   
   // Initialize FFI for desktop platforms
   sqfliteFfiInit();

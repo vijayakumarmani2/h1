cfb07fd044ec178e42353e32c695c02cc683d367
Vijay <44964055+vijayakumarmani2@users.noreply.github.com>
2025-01-27 13:13:55 +0530

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,15 @@ import 'package:hba1c_analyzer_1/screens/welcomepage.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'dart:io';
 import 'package:window_size/window_size.dart';
-import 'dart:ffi' as ffi;
+import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 
 // Declare GTK functions
 
-import 'package:window_manager/window_manager.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await windowManager.ensureInitialized();
-  WindowOptions options = WindowOptions(
-    size: Size(1920, 1080),
-    fullScreen: true,
-  );
-  windowManager.waitUntilReadyToShow(options, () async {
-    await windowManager.setFullScreen(true);
-    await windowManager.show();
-  });
-
+ 
+  
   // Initialize FFI for desktop platforms
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfi;
